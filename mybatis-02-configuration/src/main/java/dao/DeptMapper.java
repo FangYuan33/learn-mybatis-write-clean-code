60126109be741830a8dc51858669306477bb50a6
wyl <374072213@qq.com>
2021-12-04 17:03:55 +0800

    12章完毕

--- mybatis-02-configuration/src/main/java/dao/DeptMapper.java
+++ mybatis-02-configuration/src/main/java/dao/DeptMapper.java
@@ -1,7 +1,10 @@
 package dao;
 
+import dao.providers.DeptMapperProvider;
 import entity.Dept;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.SelectProvider;
 
 import java.util.List;
 import java.util.Map;
@@ -29,4 +32,16 @@ public interface DeptMapper {
      * 学习使用map更新对象
      */
     int updateByMap(Map<String, Object> map);
+
+    /**
+     * 学习使用SelectProvider
+     */
+    @SelectProvider(type = DeptMapperProvider.class, method = "findAll")
+    List<Dept> findAll();
+
+    /**
+     * 复杂SQL拼接
+     */
+    @SelectProvider(type = DeptMapperProvider.class, method = "findByEntity")
+    List<Dept> findByEntity(Dept dept);
 }
